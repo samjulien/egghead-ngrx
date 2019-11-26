f9eb5901c18c27424b053000a278dbec77e9e15f
Sam Julien <spjulien@gmail.com>
2019-11-26 15:06:12 -0800

    Initial setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,14 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+    <div class="container">
+      <h1>{{ title }}</h1>
+      <p>Stuff I still haven't done:</p>
+    </div>
+  `,
+  styles: []
 })
 export class AppComponent {
-  title = 'egghead-ngrx';
+  title = 'Todos';
 }
